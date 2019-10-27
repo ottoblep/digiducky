ac69402e356c3a863a5a8271e98de37958e52bfb
ottoblep <57066925+ottoblep@users.noreply.github.com>
2019-10-27 15:42:38 +0100

    Add files via upload

---
+++ template.ino
@@ -0,0 +1,53 @@
+// Template Script to copy parts out of
+
+#include "DigiKeyboard.h"
+void setup() {
+  // Open PowerShell
+   
+  DigiKeyboard.sendKeyStroke(0);
+  DigiKeyboard.sendKeyStroke(KEY_D, MOD_GUI_LEFT);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.print("powershell");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(2000);
+
+  // Set Layout to US
+  
+  DigiKeyboard.print("Set");
+  DigiKeyboard.sendKeyStroke(56);
+  DigiKeyboard.print("WinUserLanguageList ");
+  DigiKeyboard.sendKeyStroke(56);
+  DigiKeyboard.print("Force en");
+  DigiKeyboard.sendKeyStroke(56);
+  DigiKeyboard.print("US");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(2000);
+  DigiKeyboard.print("exit");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+
+  // Your Code Here
+  
+  // Set Layout to DE
+
+  DigiKeyboard.sendKeyStroke(0);
+  DigiKeyboard.sendKeyStroke(KEY_D, MOD_GUI_LEFT);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.print("powershell");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(2000);
+  DigiKeyboard.print("Set-WinUserLanguageList -Force de-DE");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(2000);
+  DigiKeyboard.print("exit");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  for(;;){ /*empty*/ }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
